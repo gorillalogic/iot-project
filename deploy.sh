46e606f6e66f748b096725167817863c8752d7ae
Christopher Valerio <christopher@valerio.guru>
2017-03-15 20:57:57 -0600

    Adding npm install to script

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,7 @@ WEBPACK=`which webpack`
 
 if [[ -d web ]]; then
   pushd web
+  npm install
   $WEBPACK --progress -p
   pushd src
   aws s3 cp client.min.js s3://gorilla-iot-react --acl public-read
