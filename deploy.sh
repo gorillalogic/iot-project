f3087b015f0965532b0ab25fe6b9befd078127f9
Christopher Valerio <christopher@valerio.guru>
2017-03-15 21:05:57 -0600

    Fixing npm install

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,11 @@
 #!/bin/sh 
-sudo npm install  webpack -g
+#sudo npm install  webpack -g
 
 WEBPACK=`which webpack`
 
 if [[ -d web ]]; then
   pushd web
-  npm install
+  #npm install
   $WEBPACK --progress -p
   pushd src
   aws s3 cp client.min.js s3://gorilla-iot-react --acl public-read
