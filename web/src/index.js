92a620d0844d7c62f61a5e5036d45f5d94f23d27
Fabian Montealegre <fabian.montealegre@gorillalogic.com>
2017-03-17 10:41:22 -0600

    New UI

---
+++ web/src/index.js
@@ -0,0 +1,21 @@
+/* eslint-disable import/default */
+
+import React from 'react';
+import { render } from 'react-dom';
+import { Provider } from 'react-redux'
+import { Router, browserHistory } from 'react-router';
+import routes from './routes';
+import store from './store'
+import injectTapEventPlugin from 'react-tap-event-plugin';
+require('./favicon.ico');
+import './styles.scss';
+import 'font-awesome/css/font-awesome.css';
+import 'flexboxgrid/css/flexboxgrid.css';
+
+injectTapEventPlugin();
+
+render(
+  <Provider store={store}>
+    <Router routes={routes} history={browserHistory} />
+  </Provider>, document.getElementById('app')
+);
--- web/src/reducers/index.js
+++ web/src/reducers/index.js
@@ -2,10 +2,8 @@ import { combineReducers } from 'redux'
 
 import temp from './tempReducer'
 import fan from './fanReducer'
-import app from './appReducer'
 
 export default combineReducers({
-  app,
   temp,
   fan
 })
